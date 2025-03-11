89538f9bf7ee0ca00bfacab70f5a9df59573b5fc
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-03-11 23:24:31 +0330

    add activation code model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -10,3 +10,9 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class ActivationCode(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    code = models.CharField(max_length=8, null=True, blank=True)
+    created_at = models.DateTimeField(null=True, blank=True)
