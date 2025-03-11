7881bcc49c4a9063e32353c47c2543369159da2d
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-03-12 00:05:57 +0330

    add activation code serializer

--- src/accounts/serializers.py
+++ src/accounts/serializers.py
@@ -1,6 +1,11 @@
+from rest_framework import serializers
 from djoser.serializers import UserCreateSerializer as Base
 
 
 class UserCreateSerializer(Base):
     class Meta(Base.Meta):
         fields = ['id', 'username', 'password', 'email']
+
+
+class ActivationCodeSerializer(serializers.Serializer):
+    activation_code = serializers.CharField(max_length=8)
