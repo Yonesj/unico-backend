1e210ef5f6fdadeb1c54611ac12dbe0185191fa7
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-03-12 19:27:30 +0330

    include accounts app urls

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('src.accounts.urls')),
 ]
