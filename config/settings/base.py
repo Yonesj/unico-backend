83847a847000a6ea9e3bca4ad15a8a9220c8c496
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-03-20 01:00:06 +0330

    add throttle rates

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,6 +85,16 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1000/day',
+        'user': '10000/day',
+        'account_activation': '10/day',
+        'password_reset': '10/hour',
+    },
 }
 
 SIMPLE_JWT = {
