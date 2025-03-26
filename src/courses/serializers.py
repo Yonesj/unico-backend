9553e427585dc29396676e96da601931010a668d
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-03-26 14:02:46 +0330

    add serializers

---
+++ src/courses/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from src.courses.models import Course, ClassSession, Exam, Plan
+
+
+class ClassSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClassSession
+        fields = ['day', 'start', 'end']
+
+
+class ExamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exam
+        fields = ['date', 'start', 'end']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    classes = ClassSessionSerializer(many=True, read_only=True)
+    exam = ExamSerializer(read_only=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            'course_code', 'course_name', 'theory', 'practical', 'capacity', 'gender', 'professor_name',
+            'class_location', 'prerequisites', 'notes', 'classes', 'exam'
+        ]
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    courses = CourseSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Plan
+        fields = ['name', 'courses']
+
+
+class GolestanRequestSerializer(serializers.Serializer):
+    student_id = serializers.IntegerField()
+    password = serializers.CharField()
