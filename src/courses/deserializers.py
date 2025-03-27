83c638cb162580772d3297a726640e572e2cde00
Mahdi Jafari <mahdijm.bb@gmail.com>
2025-03-27 16:18:36 +0330

    Add a function to determine the day of the class and exam time

--- src/courses/deserializers.py
+++ src/courses/deserializers.py
@@ -1,3 +1,6 @@
+import re
+import jdatetime
+
 """ Determines gender based on the input string """
 
 
@@ -38,3 +41,72 @@ def parse_prerequisites(prerequisites):
                 temp.append(item)
 
     return {"pre_needs": pre_needs, "co_needs": co_needs}
+
+
+"""Extracts class days, times, and exam date/time from the provided string."""
+
+
+def time_decomposition(time_str):
+    try:
+        start, end = map(str.strip, time_str.split('-'))
+        return {"start": start, "end": end}
+    except ValueError:
+        return {"start": -1, "end": -1}
+
+
+def determine_day(s):
+    days = {
+        'ش': 0,
+        'ي': 1,
+        'د': 2,
+        'س': 3,
+        'چ': 4,
+        'پ': 5
+    }
+    return days.get(s[0], -1)
+
+
+def times_division(class_day):
+    arr = class_day.strip().split("\n")
+    result = {
+        "times": [],
+        "exam_date": {"day": -1, "date": "0", "start": -1, "end": -1}
+    }
+
+    for item in arr:
+        if not item:
+            continue
+
+        if item[0] == 'ا':
+            match = re.search(r'امتحان\((\d{4})\.(\d{2})\.(\d{2})\)', item)
+            if match:
+                date = f"{match.group(1)}/{match.group(2)}/{match.group(3)}"
+                j_date = jdatetime.datetime.strptime(date, "%Y/%m/%d").date()
+                day_of_week = (j_date.weekday() + 1) % 7
+            else:
+                date, day_of_week = "0", -1
+
+            times = time_decomposition(item[-11:])
+            result["exam_date"] = {
+                "start": times["start"],
+                "end": times["end"],
+                "date": date,
+                "day": day_of_week
+            }
+            continue
+
+        record = {"isExerciseSolving": True}
+        temp = item.split("): ")[-1]
+        print(temp + "\n")
+        index = temp.find('-')
+
+        if index == -1:
+            continue
+
+        times = time_decomposition(temp[index - 5:index + 6])
+        record["day"] = determine_day(temp)
+        record["start"] = times["start"]
+        record["end"] = times["end"]
+        result["times"].append(record)
+
+    return result
