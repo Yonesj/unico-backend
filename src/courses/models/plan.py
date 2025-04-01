77d2598dd287c487e941a7bd311d1d9f1fb50089
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-04-02 01:37:02 +0330

    add share uuid field to plan model

--- src/courses/models/plan.py
+++ src/courses/models/plan.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from django.db import models
 from django.contrib.auth import get_user_model
 from .course import Course
@@ -6,6 +7,7 @@ from .course import Course
 User = get_user_model()
 
 class Plan(models.Model):
+    share_uuid = models.UUIDField(default=uuid4, db_index=True)
     name = models.CharField(max_length=255, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="plans")
     courses = models.ManyToManyField(Course, related_name="+")
