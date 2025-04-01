76349669073fdc3af64793323dffbbde7b27ef95
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-04-02 01:50:06 +0330

    register paths for new views

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import CourseRetrieveView
+from .views import CourseRetrieveView, PlanListCreateView, PlanDetailView, PlanRetrieveView, PlanRevokeAPIView
 
 
 urlpatterns = [
     path('courses/', CourseRetrieveView.as_view(), name='retrieve-courses-from-golestan'),
+    path('plans/<int:pk>/revoke/', PlanRetrieveView.as_view(), name='plan-link-rovoke'),
+    path('plans/<int:pk>/', PlanDetailView.as_view(), name='plan-detail'),
+    path('plans/<uuid:share_uuid>/', PlanRevokeAPIView.as_view(), name='retrieve-shared-plan'),
+    path('plans/', PlanListCreateView.as_view(), name='retrieve-create-plans'),
 ]
