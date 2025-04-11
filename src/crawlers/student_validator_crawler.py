6f42145b5f86b378b5d7bb1229f0f16d74a230cf
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-04-11 22:00:26 +0330

    add student validator crawler

--- src/crawlers/student_validator_crawler.py
+++ src/crawlers/student_validator_crawler.py
@@ -1,10 +1,10 @@
 import time
+from .golestan_base_crawler import GolestanBaseCrawler
 
 
-class UserGolestanValidator:
-    def user_validator(self, page):
-        # Navigate to student info section
-        iframe2 = page.frame_locator("iframe#Faci2")
+class StudentValidatorCrawler(GolestanBaseCrawler):
+    def __navigate_to_student_info_page(self):
+        iframe2 = self.page.frame_locator("iframe#Faci2")
         form_body2 = iframe2.frame_locator("frame[name='Master']").frame_locator("frame[name='Form_Body']")
         student_info_btn = form_body2.locator('//td[span[text()="اطلاعات جامع دانشجو"]]')
         student_info_btn.wait_for(state="visible", timeout=5000)
@@ -12,7 +12,8 @@ class UserGolestanValidator:
         time.sleep(0.5)
         student_info_btn.click()
 
-        iframe3 = page.frame_locator("iframe#Faci3")
+    def __extract_student_info(self):
+        iframe3 = self.page.frame_locator("iframe#Faci3")
         form_body3 = iframe3.frame_locator("frame[name='Master']").frame_locator("frame[name='Form_Body']")
 
         student_name_el = form_body3.locator('label#F51851')
@@ -30,10 +31,15 @@ class UserGolestanValidator:
         major = student_major_el.text_content()
         faculty = student_faculty_el.text_content()
 
-
         return {
             "student_number": student_number,
             "full_name": student_fullname,
             "major": major,
             "faculty": faculty
         }
+
+    def fetch_student_info(self, username, password):
+        """ Main function to fetch student courses. """
+        self.login(username, password)
+        self.__navigate_to_student_info_page()
+        return self.__extract_student_info()
