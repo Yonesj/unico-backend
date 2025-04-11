7ce9d1a1556a85d941bcd925f71714382f3cd882
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-04-11 21:44:24 +0330

    apply oop concepts. refactors: 1. move login logic to a base class to reduce redundancy by inheritance 2. make base class abstract 3. make helper methods private

--- src/crawlers/golestan_base_crawler.py
+++ src/crawlers/golestan_base_crawler.py
@@ -1,22 +1,23 @@
 import base64
+from abc import ABC
 from django.utils.translation import gettext_lazy as _
 from playwright.sync_api import sync_playwright
 from src.crawlers.captcha_solver.captcha_solver import solve
 
 
-class Crawler:
+class GolestanBaseCrawler(ABC):
     def __init__(self):
         self.playwright = sync_playwright().start()
         self.browser = self.playwright.chromium.launch(headless=True)
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
 
-    def navigate_to_login_page(self):
+    def __navigate_to_login_page(self):
         """ Open the login page and wait for it to load. """
         self.page.goto("https://golestan.ui.ac.ir/forms/authenticateuser/main.htm")
         self.page.wait_for_load_state("load")
 
-    def extract_captcha(self):
+    def __extract_captcha(self):
         """ Extract the captcha image as a base64 string. """
         iframe_locator = self.page.frame_locator("iframe#Faci1")
         form_body_frame = iframe_locator.frame_locator("frame[name='Master']").frame_locator("frame[name='Form_Body']")
@@ -24,7 +25,7 @@ class Crawler:
         captcha_byte = captcha_element.screenshot()
         return base64.b64encode(captcha_byte).decode("utf-8")
 
-    def submit_login(self, username, password, captcha_text):
+    def __submit_login(self, username, password, captcha_text):
         """ Fill in login details and submit the form. """
         iframe_locator = self.page.frame_locator("iframe#Faci1")
         form_body_frame = iframe_locator.frame_locator("frame[name='Master']").frame_locator("frame[name='Form_Body']")
@@ -42,7 +43,7 @@ class Crawler:
         login_button.click()
         self.page.wait_for_load_state("networkidle")
 
-    def check_login_status(self):
+    def __check_login_status(self):
         """ Check if login was successful or failed (captcha or wrong password). """
         iframe_locator = self.page.frame_locator("iframe#Faci1")
         message_frame = iframe_locator.frame_locator("frame[name='Message']")
@@ -68,86 +69,27 @@ class Crawler:
 
         raise Exception("unknown error")
 
-    def search_courses(self):
-        """ Search for available courses using code 212. """
-        iframe_locator2 = self.page.frame_locator("iframe#Faci2")
-        form_body_frame2 = iframe_locator2.frame_locator("frame[name='Master']").frame_locator("frame[name='Form_Body']")
-
-        search_button = form_body_frame2.locator('//*[@id="F20851"]')
-        search_button.wait_for(state="visible")
-        search_button.fill("212")
-
-        search_click_button = form_body_frame2.locator('//*[@id="OK"]')
-        search_click_button.wait_for(state="visible")
+    def login(self, username, password):
+        """ Main function to login in golestan. """
         max_tries = 20
 
         while max_tries > 0:
-            if self.page.locator("iframe#Faci3").count() > 0:
-                return
-            search_click_button.click()
-            self.page.wait_for_timeout(100)
-            max_tries -= 1
-
-        raise Exception("Couldn't find the 212 report page after multiple attempts.")
-
-    def extract_courses(self):
-        """ Extract the course list from the page and return as structured data. """
-        iframe_locator3 = self.page.frame_locator("iframe#Faci3")
-        commander = iframe_locator3.frame_locator("frame[name='Commander']")
-
-        with self.page.expect_popup() as new_tab_info:
-            self.page.wait_for_timeout(100)
-            commander.locator('//*[@id="ExToEx"]').click()
-        course_page = new_tab_info.value
-
-        course_page.wait_for_selector("table")
-        main_table = course_page.locator("table").first
-        rows = main_table.locator("> tbody > tr")
-        all_data = rows.all_inner_texts()
-        student_course_data_list = []
-
-        for i in range(1, len(all_data)):
-            course_data = all_data[i].split('\t')
-            student_course_data_list.append({
-                'course_code': course_data[3],
-                'course_name': course_data[4],
-                'theory': course_data[5],
-                'practical': course_data[6],
-                'capacity': course_data[7],
-                'gender': course_data[8],
-                'professor_name': course_data[9],
-                'classes': course_data[10],
-                'class_location': course_data[11],
-                'prerequisites': course_data[12:-1],
-                'notes': course_data[-1]
-            })
-
-        return student_course_data_list
-
-    def fetch_student_courses(self, username, password):
-        """ Main function to fetch student courses. """
-        max_tries = 20
-
-        while max_tries > 0:
-            self.navigate_to_login_page()
-            captcha_b64 = self.extract_captcha()
+            self.__navigate_to_login_page()
+            captcha_b64 = self.__extract_captcha()
 
             try:
                 captcha_text = solve(captcha_b64)
             except Exception:
                 continue
 
-            self.submit_login(username, password, captcha_text)
-            if self.check_login_status():
+            self.__submit_login(username, password, captcha_text)
+            if self.__check_login_status():
                 break
 
             max_tries -= 1
 
         if max_tries == 0:
             raise ValueError(_("Login failed"))
-
-        self.search_courses()
-        return self.extract_courses()
 
     def close(self):
         """ Clean up Playwright resources. """
