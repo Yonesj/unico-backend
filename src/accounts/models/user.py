65ccff2c9f572d5e22aec66f0a7aace81f4cfc45
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-04-11 21:55:44 +0330

    alter fields of user model

--- src/accounts/models/user.py
+++ src/accounts/models/user.py
@@ -3,7 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    first_name = None
+    last_name = None
     email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=256, null=True, blank=True)
+    is_ui_student = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
