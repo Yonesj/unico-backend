387edfe932ac6d47962efdbfc5f563f50eb447b6
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-05-07 21:50:04 +0330

    add professor proposal create view

---
+++ src/reviews/views/professor_proposal_view.py
@@ -0,0 +1,11 @@
+# from rest_framework.generics import CreateAPIView
+# from rest_framework.permissions import IsAuthenticated
+#
+# from src.reviews.models import ProfessorProposal
+# from src.reviews.serializers import ProfessorProposalCreateSerializer
+#
+#
+# class ProfessorProposalCreateView(CreateAPIView):
+#     # permission_classes = [IsAuthenticated]
+#     serializer_class = ProfessorProposalCreateSerializer
+#     queryset = ProfessorProposal.objects.all()
