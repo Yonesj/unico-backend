0138f9650c9b9e11013e9582a665405aa73ba10e
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-05-07 21:15:22 +0330

    add django filter to installed apps list

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'djoser',
     'corsheaders',
     'src.accounts',
