20c41b409c2d12235b8d08f10b7c21f2d6f0d782
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-05-08 01:34:50 +0330

    document course views

--- src/reviews/views/course_view.py
+++ src/reviews/views/course_view.py
@@ -5,8 +5,10 @@ from rest_framework.permissions import AllowAny
 
 from src.reviews.models import Course
 from src.reviews.serializers import CourseSummarySerializer
+from src.reviews.schemas import course_list_view_schema
 
 
+@course_list_view_schema
 class CourseListView(ListAPIView):
     permission_classes = [AllowAny]
     queryset = Course.objects.all()
