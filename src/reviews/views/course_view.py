5975cf1671cd93bf44db8bb7163d12f72aa1b009
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-05-07 19:36:38 +0330

    implement course list view

---
+++ src/reviews/views/course_view.py
@@ -0,0 +1,17 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny
+
+from src.reviews.models import Course
+from src.reviews.serializers import CourseSummarySerializer
+
+
+class CourseListView(ListAPIView):
+    permission_classes = [AllowAny]
+    queryset = Course.objects.all()
+    serializer_class = CourseSummarySerializer
+
+    filter_backends = [SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['faculty_id']
+    search_fields = ['name']
