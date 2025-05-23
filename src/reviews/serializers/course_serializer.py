9d72de38f67ee9dd6da32dbe0045202f7ab93e37
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-05-23 22:22:21 +0330

    refactor course create serializer based on new fields

--- src/reviews/serializers/course_serializer.py
+++ src/reviews/serializers/course_serializer.py
@@ -1,8 +1,7 @@
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
 
-from src.reviews.models import Course
+from src.reviews.models import Course, State
 
 
 class CourseSummarySerializer(serializers.ModelSerializer):
@@ -13,14 +12,21 @@ class CourseSummarySerializer(serializers.ModelSerializer):
 
 
 class CourseCreateSerializer(serializers.ModelSerializer):
+    state = serializers.HiddenField(default=State.PENDING)
+
     class Meta:
         model = Course
-        fields = ['id', 'professor', 'name', 'created_at']
-        read_only_fields = ['id', 'created_at']
-        validators = [
-            UniqueTogetherValidator(
-                queryset=Course.objects.all(),
-                fields=['professor', 'name'],
-                message=_("This professor already has a course with that name")
-            )
-        ]
+        fields = ['id', 'professor', 'name', 'state']
+        read_only_fields = ['id']
+
+    def validate(self, attrs):
+        professor = attrs.get('professor')
+        name = attrs.get('name')
+
+        if Course.objects.filter(state=State.APPROVED, professor=professor, name=name).exists():
+            raise serializers.ValidationError({
+                'non_field_errors': [_("This professor already has a course with that name")]
+            })
+
+        attrs['faculty'] = professor.faculty
+        return super().validate(attrs)
