225c880635d4908e624175fa68bba3432195d9a6
Mahdi Jafari <mahdijm.bb@gmail.com>
2025-05-31 14:31:28 +0330

    change of view of create message url

--- src/tickets/urls.py
+++ src/tickets/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from src.tickets.views import (
     TicketsListCreateView,
     TicketStatsView,
-    TicketChatView
+    TicketChatView,
+    MessageCreateView
 )
 
 urlpatterns = [
     path('', TicketsListCreateView.as_view(), name='list-create-tickets'),
     path('stats/', TicketStatsView.as_view(), name='ticket-stats'),
-    path('<int:pk>/messages/', TicketChatView.as_view(), name='create-message'),
+    path('<int:pk>/messages/', MessageCreateView.as_view(), name='create-message'),
     path('<int:pk>/', TicketChatView.as_view(), name='retrieve-chat-panel'),
 ]
+
