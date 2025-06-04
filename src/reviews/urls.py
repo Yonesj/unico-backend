36e1b788a0069f5921c1775eb808f02d86988802
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-06-04 14:51:45 +0330

    register url for the new endpoint

--- src/reviews/urls.py
+++ src/reviews/urls.py
@@ -11,7 +11,7 @@ from .views import (
     ReviewReactionCreateView,
     ReviewReportCreateView,
     CourseListCreateView,
-    MyReviewListView,
+    MyReviewsByProfessorView,
     ReviewCreateView,
     ReviewReactionUpdateDestroyView,
     ReviewRetrieveView,
@@ -20,7 +20,8 @@ from .views import (
     ProfessorCardRetrieveView,
     FacultyListView,
     ProfessorProposalCreateView,
-    ProfessorRevisionCreateView
+    ProfessorRevisionCreateView,
+    UserReviewListView
 )
 
 
@@ -29,7 +30,7 @@ urlpatterns = [
 
     path('faculties/', FacultyListView.as_view(), name='list-faculty'),
 
-    path('courses/', CourseListCreateView.as_view(), name='list-course'),
+    path('courses/', CourseListCreateView.as_view(), name='list-create-course'),
     # path('courses/<int:pk>/', TempView.as_view(), name='retrieve-update-course'),
     # path('courses/<int:course_pk>/reviews/', TempView.as_view(), name='list-and-create-review'),
 
@@ -42,10 +43,11 @@ urlpatterns = [
     path('professors/<int:pk>/', ProfessorRetrieveView.as_view(), name='retrieve-professor'),
     path('professors/<int:pk>/revisions/', ProfessorRevisionCreateView.as_view(), name='edit-professor'),
     path('professors/<int:pk>/reviews/', ProfessorReviewsListView.as_view(), name='retrieve-professor-reviews'),
-    path('professors/<int:pk>/reviews/me/', MyReviewListView.as_view(), name='retrieve-user-professor-reviews'),
+    path('professors/<int:pk>/reviews/me/', MyReviewsByProfessorView.as_view(), name='retrieve-user-professor-reviews'),
     path('professors/<int:pk>/reactions/me/', MyReviewReactionListView.as_view(), name='retrieve-my-review-reactions'),
 
     path('reviews/', ReviewCreateView.as_view(), name='create-review'),
+    path('reviews/me/', UserReviewListView.as_view(), name='user-review-list'),
     path('reviews/latest/', LatestReviewListView.as_view(), name='latest-reviews'),
     path('reviews/<int:pk>/', ReviewRetrieveView.as_view(), name='retrieve-review'),
     path('reviews/<int:pk>/reactions/', ReviewReactionCreateView.as_view(), name='create-review-reaction'),
